b1ac6e400c94e32c5dd4da17d5288a0b7976638f
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-01 22:04:46 +0200

    Added the rest of the UI and added questions as scriptable objects

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e9d8e4c5a9baf8ef222b59df3b39aec1cd4b897a704174352253d0f5c29a7c6a
+size 657
---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d2a43c6ad0e5ca1628b39fa9a31cdef4e186e5b6c5c4ffbaf02bb4497a608c37
+size 605
